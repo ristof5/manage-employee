692d95598cb730ba0c6dcb37132ef6869dc1d10b
riffani <sidikriffani481@gmail.com>
2025-10-02 15:41:57 +0700

    feat(backend): update route for departements

--- backend/routes/employees.js
+++ backend/routes/employees.js
@@ -1,8 +1,14 @@
-const express = require ("express");
+const express = require("express");
 const router = express.Router();
-const employeeController = require("../controllers/employeeController")
+const employeeController = require("../controllers/employeeController");
 
+// Penting! Route untuk GET all (dengan pagination/search)
 router.get("/", employeeController.getAll);
+
+// Untuk dropdown department
+router.get("/departments", employeeController.getDepartments);
+
+// ...route lain
 router.get("/:id", employeeController.getById);
 router.post("/", employeeController.create);
 router.put("/:id", employeeController.update);
